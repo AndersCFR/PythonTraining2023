5d88d1bf0491bebff7b94784a35ba7dca98fd63a
AndersCFR <anderson.cardenas.r@gmail.com>
2023-09-06 22:23:15 -0500

    feat: ejercicio

---
+++ Ejercicios_Proyectos/1.Calculadora.py
@@ -0,0 +1,7 @@
+'''
+Elaborar una calculadora que solicite al usuario dos números
+
+Presentar al usuario la suma, resta, multiplicación, división, la potencia dos de amvos numeros ingresados
+La raíz cuadrada de ambos números, La raiz cúbica de ambos número ingresados, 
+la división entera de entre ambos números
+'''
