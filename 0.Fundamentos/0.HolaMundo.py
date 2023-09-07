dc483762dee67ba4d3f5ab6b5009f88d37f7a5f9
AndersCFR <anderson.cardenas.r@gmail.com>
2023-09-06 21:38:54 -0500

    feat:variables

---
+++ 0.Fundamentos/0.HolaMundo.py
@@ -0,0 +1,22 @@
+# Primer programa
+'''
+Elaborar un formulario de datos en python
+TODO: Crear formulario
+'''
+
+print('\tFormulario')
+
+print('1. Nombre: ', end='')
+nombre = input()
+
+print('2. Apellido: ', end='')
+apellido = input()
+
+print('3. Edad: ', end='')
+edad = input()
+
+print('\tResultados')
+print('El nombre ingresado es:', nombre)
+print('El apellido ingresado es:', apellido)
+print('La edad ingresada es:', edad)
+ 
---
+++ 0.Fundamentos/1.Variables.py
@@ -0,0 +1,11 @@
+# Variables
+
+edad = 24
+peso = 48.5
+nombre = 'Anderson'
+mayorEdad = True
+
+print('El tipo de dato de la variables edad es: ', type(edad))
+print('El tipo de dato de la variables peso es: ', type(peso))
+print('El tipo de dato de la variables nombres es: ', type(nombre))
+print('El tipo de dato de la variable mayorEdad es:', type(mayorEdad))
