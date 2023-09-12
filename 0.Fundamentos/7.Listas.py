369d09c52514c96fbef24e9efce1c8df9695e7d3
AndersCFR <anderson.cardenas.r@gmail.com>
2023-09-11 21:41:53 -0500

    feat:listas

---
+++ 0.Fundamentos/7.Listas.py
@@ -0,0 +1,58 @@
+# Listas 
+# list()
+# Inicialización []
+# Mutable
+
+
+lista_numeros = [1,2,3,4,5,6,7,8,9]
+print(type(lista_numeros))
+
+# Lista de varios tipos de datos
+listas_variada = [1,2, 3, 4, 5, 'a', 'b', 'c', True, 34.6]
+print(listas_variada)
+
+# Métodos y operaciones
+
+# Size
+print(len(listas_variada))
+
+# Indexación
+print(lista_numeros[0:2])
+print(lista_numeros[4])
+
+# Mutable
+listas_variada[0] = 0
+print(listas_variada)
+
+print(listas_variada[::-1])
+
+lista2 = listas_variada[3:6]
+print(lista2)
+
+# Métodos
+
+listaAlumnos = ['Anderson', 'Luis', 'Anita']
+
+# Append, agregar datos al final
+listaAlumnos.append('Pepito')
+print(listaAlumnos)
+
+#Insert, agregar un dato auna posición específica
+listaAlumnos.insert(3, 'Juanita')
+print(listaAlumnos)
+
+# Extend
+listaAlumnos.extend(['Pablito', 'María'])
+print(listaAlumnos)
+
+# Retirar valores al final de la lista
+listaAlumnos.pop()
+print(listaAlumnos)
+
+# Retirar con pop en un índice específico
+listaAlumnos.pop(1)
+print(listaAlumnos)
+
+# Retir un valor de la lista
+listaAlumnos.remove('Pablito')
+print(listaAlumnos)
---
+++ Ejercicios_Proyectos/3.Listas.py
@@ -0,0 +1,6 @@
+# 1. Crear una lista con los dos últimos valores de la lista
+
+# 2. Crear una lista con los mismos valores pero invertidos
+
+# 3. Crear una lista que contenga la mitad de elementos de la lista original
+# [1,2,3,4,5,6,7,8,9,10]  -> 10/2 = 5  -> [0:6]
