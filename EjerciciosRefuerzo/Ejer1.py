7a2b06e111af106ca81736a83717f63791f3ff64
AndersCFR <anderson.cardenas.r@gmail.com>
2023-09-21 21:29:24 -0500

    feat: tres ejercicios

---
+++ EjerciciosRefuerzo/Ejer1.py
@@ -0,0 +1,2 @@
+# Solicitar a un usuario que ingrese el número de elementos que requiere para crear una lista
+# Llenar cada uno de los valores con los datos solicitados al usario
---
+++ EjerciciosRefuerzo/Ejer2.py
@@ -0,0 +1,3 @@
+# Solicitar al usuario que ingresa su dirección de domicilio
+# Separar en una lista aquellos valores que sean vocales
+# Seperar en otra lista los valores de consonantes
