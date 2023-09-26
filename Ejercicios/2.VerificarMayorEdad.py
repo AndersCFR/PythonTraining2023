1d3e3f25a762ea3f6a4cc4ef7ea9467c1d9220e8
AndersCFR <anderson.cardenas.r@gmail.com>
2023-09-25 21:35:07 -0500

    feat: ejercicio 2

---
+++ Ejercicios/2.VerificarMayorEdad.py
@@ -0,0 +1,4 @@
+# Solictar al usuario que ingrese su edad
+# Si es mayor o igual a 18 años decir que es mayor de edad
+# Caso contrario indicar que el usuario es menor de edad
+# Para ello crear una función
